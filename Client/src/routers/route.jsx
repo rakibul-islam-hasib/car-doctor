697ad4ad16dc6403efaadf191b6fe21bba20566c
Hasib Hossain <boraborhasib@gmail.com>
2023-05-10 07:34:35 +0600

    Init Commit

---
+++ Client/src/routers/route.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+
+export const router = createBrowserRouter([
+    {
+        path : '/', 
+        element : <App />, 
+
+    }
+])
