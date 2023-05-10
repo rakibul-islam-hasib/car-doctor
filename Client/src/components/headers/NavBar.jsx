61985e68fa805a4d30fa6bea79e26d38e93c37bb
Hasib Hossain <boraborhasib@gmail.com>
2023-05-10 08:21:56 +0600

    Banner added

--- Client/src/components/headers/NavBar.jsx
+++ Client/src/components/headers/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../../assets/logo.svg';
+
 const NavBar = () => {
     const navItems = [
         {
---
+++ Client/src/components/pages/home/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import img1 from '../../../assets/images/banner/1.jpg';
+import img2 from '../../../assets/images/banner/2.jpg';
+import img3 from '../../../assets/images/banner/3.jpg';
+import img4 from '../../../assets/images/banner/4.jpg';
+import img5 from '../../../assets/images/banner/5.jpg';
+import img6 from '../../../assets/images/banner/6.jpg';
+
+const Banner = () => {
+    return (
+        <div className='h-[70vh]' style={{backgroundImage : `url(${img1})`}}>
+            
+        </div>
+    );
+};
+
+export default Banner;
