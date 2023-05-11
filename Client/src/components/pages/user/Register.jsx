525289560bd015761e1e065571ff78d42f84b554
Hasib Hossain <boraborhasib@gmail.com>
2023-05-11 08:12:50 +0600

    Server Side Added

--- Client/src/components/pages/user/Register.jsx
+++ Client/src/components/pages/user/Register.jsx
@@ -2,85 +2,91 @@ import React, { useContext } from 'react';
 import NavBar from '../../headers/NavBar';
 import img from '../../../assets/images/login/login.svg';
 import { Link } from 'react-router-dom';
-
+import { AuthContext } from '../../../context/AuthProviders';
+import { FadeLoader } from 'react-spinners'
 const Register = () => {
-    const handelFormSubmit = e => { 
+    const { createUser, loader } = useContext(AuthContext)
+
+    const handelFormSubmit = e => {
         e.preventDefault();
         const name = e.target.name.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(name, email, password)
-        // createUser(email, password)
-        // .then(res => { 
-        //     console.log(res.user)
-        // })
-        // .catch(err => { 
-        //     console.log(err)
-        // })
+        createUser(email, password)
+            .then(res => {
+                console.log(res.user)
+            })
+            .catch(err => {
+                console.log(err)
+            })
     }
     return (
-        <div>
-            <div className='w-[85%] mx-auto'>
-                <NavBar />
-                <div className="hero min-h-screen ">
-                    <div className="hero-content flex-col lg:flex-row">
-                        <div className="mr-8">
-                            <img src={img} alt="" />
-                        </div>
-                        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                            <div className="card-body">
-                                <h1 className="text-4xl font-bold">Register now!</h1>
-                                <form onSubmit={handelFormSubmit}>
-                                    <div className="form-control">
-                                        <label className="label">
-                                            <span className="label-text">Name</span>
-                                        </label>
-                                        <input type="text" required name='name' placeholder="Name" className="input input-bordered" />
+        loader ? <div className="h-screen flex justify-center items-center">
+            <FadeLoader color="#36d7b7" />
+        </div> :
+            <div>
+                <div className='w-[85%] mx-auto'>
+                    <NavBar />
+                    <div className="hero min-h-screen ">
+                        <div className="hero-content flex-col lg:flex-row">
+                            <div className="mr-8">
+                                <img src={img} alt="" />
+                            </div>
+                            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                                <div className="card-body">
+                                    <h1 className="text-4xl font-bold">Register now!</h1>
+                                    <form onSubmit={handelFormSubmit}>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text">Name</span>
+                                            </label>
+                                            <input type="text" required name='name' placeholder="Name" className="input input-bordered" />
+                                        </div>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text">Email</span>
+                                            </label>
+                                            <input type="email" name='email' required placeholder="email" className="input input-bordered" />
+                                        </div>
+                                        <div className="form-control">
+                                            <label className="label">
+                                                <span className="label-text">Password</span>
+                                            </label>
+                                            <input type="password" name='password' required placeholder="password" className="input input-bordered" />
+                                        </div>
+                                        <div className="form-control mt-6">
+                                            <button className="btn btn-primary">Register</button>
+                                        </div>
+                                    </form>
+                                    <div className="divider">Or</div>
+                                    <div className="grid grid-cols-2 gap-4">
+                                        <button className="btn btn-ghost btn-sm btn-block">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="icon text-primary icon-tabler icon-tabler-brand-facebook" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" />
+                                                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4V8a1 1 0 0 1 1 -1h3V3h-3a5 5 0 0 0 -5 5v2H7" />
+                                            </svg>
+                                            <span className="ml-2">Facebook</span>
+                                        </button>
+                                        <button className="btn btn-ghost btn-sm btn-block">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-google" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" />
+                                                <path d="M17.788 5.108A9 9 0 1021 12h-8" />
+                                            </svg>
+                                            <span className="ml-2">Google</span>
+                                        </button>
                                     </div>
-                                    <div className="form-control">
-                                        <label className="label">
-                                            <span className="label-text">Email</span>
-                                        </label>
-                                        <input type="email" name='email' required placeholder="email" className="input input-bordered" />
-                                    </div>
-                                    <div className="form-control">
-                                        <label className="label">
-                                            <span className="label-text">Password</span>
-                                        </label>
-                                        <input type="password" name='password' required placeholder="password" className="input input-bordered" />
-                                    </div>
-                                    <div className="form-control mt-6">
-                                        <button className="btn btn-primary">Register</button>
-                                    </div>
-                                </form>
-                                <div className="divider">Or</div>
-                                <div className="grid grid-cols-2 gap-4">
-                                    <button className="btn btn-ghost btn-sm btn-block">
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="icon text-primary icon-tabler icon-tabler-brand-facebook" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" />
-                                            <path d="M7 10v4h3v7h4v-7h3l1 -4h-4V8a1 1 0 0 1 1 -1h3V3h-3a5 5 0 0 0 -5 5v2H7" />
-                                        </svg>
-                                        <span className="ml-2">Facebook</span>
-                                    </button>
-                                    <button className="btn btn-ghost btn-sm btn-block">
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-google" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" />
-                                            <path d="M17.788 5.108A9 9 0 1021 12h-8" />
-                                        </svg>
-                                        <span className="ml-2">Google</span>
-                                    </button>
-                                </div>
-                                <p className='text-center'><small>Already have an account ?
-                                    <Link
-                                        className='text-primary font-bold'
-                                        to={'/login'}>Login</Link></small></p>
+                                    <p className='text-center'><small>Already have an account ?
+                                        <Link
+                                            className='text-primary font-bold'
+                                            to={'/login'}>Login</Link></small></p>
 
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
     );
 };
 
--- Client/src/context/AuthProviders.jsx
+++ Client/src/context/AuthProviders.jsx
@@ -1,11 +1,37 @@
-import React, { createContext } from 'react';
-
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { app } from '../firebase/firebase.init';
 export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
 const AuthProviders = ({ children }) => {
+    const [loader, setLoader] = useState(true);
+    const [user, setUser] = useState(null);
+    // * Register 
     const createUser = (email, password) => {
-        
+        setLoader(true);
+        return createUserWithEmailAndPassword(auth, email, password);
     }
-    const conValue = 'HI'
+
+    // * Login 
+    const login = (email, password) => {
+        setLoader(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+
+
+
+    //  * Make a observer for user login or logout 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth , user => {
+            setUser(user);
+            setLoader(false);
+        })
+        return () => unsubscribe();
+    }, [])
+    // console.log(user)
+    const conValue = { createUser, user, loader , login , setLoader };
     return (
         <AuthContext.Provider value={conValue}>
             {children}
